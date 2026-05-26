8f3123cbbec15bc8db454b571819f6916e6db886
Sergio Esteban <sesteban@caltech.edu>
2026-05-25 18:34:28 -0700

    added empty g1 files before we try any tasks

--- mjpc/tasks/tasks.cc
+++ mjpc/tasks/tasks.cc
@@ -26,6 +26,7 @@
 #include "mjpc/tasks/cartpole/cartpole.h"
 #include "mjpc/tasks/cartpole_walls/cartpole_walls.h"
 #include "mjpc/tasks/fingers/fingers.h"
+#include "mjpc/tasks/g1/stand/stand.h"
 #include "mjpc/tasks/humanoid/interact/interact.h"
 #include "mjpc/tasks/humanoid/stand/stand.h"
 #include "mjpc/tasks/humanoid/tracking/tracking.h"
@@ -54,6 +55,7 @@ std::vector<std::shared_ptr<Task>> GetTasks() {
       std::make_shared<Cartpole>(),
       std::make_shared<CartpoleWalls>(),
       std::make_shared<Fingers>(),
+      std::make_shared<g1::Stand>(),
       std::make_shared<humanoid::Interact>(),
       std::make_shared<humanoid::Stand>(),
       std::make_shared<humanoid::Tracking>(),
