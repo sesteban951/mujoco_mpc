163ca9c55b895622486bc5ac96e3b994646706d2
sesteban951 <sesteban@caltech.edu>
2026-05-27 18:35:05 -0700

    added some comments so that I can understand the standing task

--- mjpc/tasks/g1/stand/stand.cc
+++ mjpc/tasks/g1/stand/stand.cc
@@ -1,17 +1,3 @@
-// Copyright 2026 DeepMind Technologies Limited
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 #include "mjpc/tasks/g1/stand/stand.h"
 
 #include <string>
@@ -21,9 +7,12 @@
 
 namespace mjpc::g1 {
 
+// convinient accessor for model path
 std::string Stand::XmlPath() const {
   return GetModelPath("g1/stand/task.xml");
 }
+
+//convenient accessor for task name
 std::string Stand::Name() const { return "G1 Stand"; }
 
 // ------------------ Residuals for G1 stand task ------------------
